6d900493f7a634dc638f5bdf9adb61c561b55116
Paco <fztmon@gmail.com>
2021-08-30 18:03:03 -0500

    benchmarking

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <vector>
 #include "Lexer.h"
+#include <chrono>
 int main() {
    std::cout << "**Scheme Lexer**\n";
    Lexer lexer;
+   //benchmark lexer using timer
+   auto start = std::chrono::high_resolution_clock::now();
    lexer.lexerScheme("./testfile.sch");
+   auto end = std::chrono::high_resolution_clock::now();
+   std::cout << "Tiempo de ejecución: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " milliseconds\n";
 }
